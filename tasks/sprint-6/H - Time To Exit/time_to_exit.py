ccb003db9eae2aac742856c8bbbcd08340adbec8
Sergey Goppikov <grey2k@gmail.com>
2021-08-29 13:03:38 +0300

    Time to Exit - finish

--- tasks/sprint-6/H - Time To Exit/time_to_exit.py
+++ tasks/sprint-6/H - Time To Exit/time_to_exit.py
@@ -1,6 +1,5 @@
 # from typing import Dict, List
 # from draw import nx, draw
-from collections import deque
 
 COLOR_WHITE = 'white'
 COLOR_GRAY = 'gray'
@@ -11,45 +10,51 @@ def main():
     n, m = map(int, input().strip().split())
 
     adjacency_list = {(n + 1): [] for n in range(n)}  # type Dict[int, List[int]]
-    colors = {(n + 1): COLOR_WHITE for n in range(n)}
+    colors = [COLOR_WHITE for n in range(n + 1)]
+    entry = [0 for n in range(n + 1)]
+    leave = [0 for n in range(n + 1)]
 
-    # graph = nx.Graph()
+    # graph = nx.DiGraph()
     # graph.add_nodes_from(range(1, n + 1))
 
     for _ in range(m):
         u, v = map(int, input().strip().split())
         # graph.add_edge(u, v)
         adjacency_list.get(u).append(v)
-        adjacency_list.get(v).append(u)
 
-    start = int(input().strip())
+    start = 1
 
     # print(start)
     # print(adjacency_list)
     # print(colors)
-
+    #
     # draw(graph)
 
-    distance = {start: 0}
-    bfs(start, adjacency_list, colors, distance)
-    print(str(max(distance.values())))
+    dfs_stack(start, adjacency_list, colors, entry, leave)
+
+    for i in range(1, n + 1):
+        print(f"{entry[i]} {leave[i]}")
 
 
-def bfs(start: int, graph: dict, colors: dict, distance: dict) -> None:
-    planned = deque([start])
-    previous = {}
-    colors[start] = COLOR_GRAY
+def dfs_stack(start: int, graph: dict, colors: list, entry: list, leave: list) -> None:
+    stack = [start]
+    time = -1
+    while len(stack) > 0:
+        v = stack.pop()
+        if colors[v] == COLOR_WHITE:
+            time += 1
+            entry[v] = time
 
-    while len(planned) > 0:
-        u = planned.pop()
-        for v in sorted(graph.get(u)):
-            if colors[v] == COLOR_WHITE:
-                distance[v] = distance[u] + 1
-                previous[v] = u
-                colors[v] = COLOR_GRAY
-                planned.appendleft(v)
+            colors[v] = COLOR_GRAY
+            stack.append(v)
 
-        colors[u] = COLOR_BLACK
+            for node in sorted(graph.get(v))[::-1]:
+                if colors[node] == COLOR_WHITE:
+                    stack.append(node)
+        elif colors[v] == COLOR_GRAY:
+            colors[v] = COLOR_BLACK
+            time += 1
+            leave[v] = time
 
 
 if __name__ == '__main__':
