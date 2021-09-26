085fb242e42ab7d686d0e59d96fc53b2df4ac4a9
Sergey Goppikov <grey2k@gmail.com>
2021-09-26 14:42:41 +0300

    Sprint 8 - 5 done

--- tasks/sprint-8/H - Search With a Shift/search_with_shift.py
+++ tasks/sprint-8/H - Search With a Shift/search_with_shift.py
@@ -34,7 +34,7 @@ def find_all(search: list, pattern: list) -> list:
             break
 
         occurrences.append(pos)
-        start = pos + 1
+        start = pos
 
     return occurrences
 
@@ -48,20 +48,20 @@ def find(search: list, pattern: list, start: int) -> int:
     if len(search) - start < len(pattern):
         return result
 
-    for pos in range(start, len(search) - len(pattern)):
+    for pos in range(start, len(search) - len(pattern) + 1):
         shift = None
         match = True
 
         for offset in range(len(pattern)):
             if shift is None:
-                shift = search[pos] - pattern[offset]
+                shift = pattern[offset] - search[pos]
 
             if search[pos + offset] + shift != pattern[offset]:
                 match = False
                 break
 
         if match:
-            result = pos
+            result = pos + 1
             break
 
     return result
