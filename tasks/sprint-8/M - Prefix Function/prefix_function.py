1f07273cef7696fc507c3b546db6f2a77f4fb8db
Sergey Goppikov <grey2k@gmail.com>
2021-09-26 20:17:26 +0300

    Prefix function done

--- tasks/sprint-8/M - Prefix Function/prefix_function.py
+++ tasks/sprint-8/M - Prefix Function/prefix_function.py
@@ -1,70 +1,26 @@
 def main():
-    n = int(input().strip())
+    string = input().strip()
 
-    try:
-        search = list(map(int, input().strip().split()[:n]))
-    except EOFError:
-        search = []
-
-    m = int(input().strip())
-
-    try:
-        pattern = list(map(int, input().strip().split()[:m]))
-    except EOFError:
-        pattern = []
-
-    print(' '.join(map(str, find_all(search, pattern))))
+    print(' '.join(map(str, prefix_function(string))))
 
 
-def find_all(search: list, pattern: list) -> list:
-    occurrences = []
+def prefix_function(s: str) -> list:
+    p = [0] * len(s)
+    j = 0
+    i = 1
 
-    len_pattern = len(pattern)
-    len_search = len(search)
+    while i < len(s):
+        if s[j] != s[i]:
+            if j > 0:
+                j = p[j - 1]
+            else:  # j == 0
+                i += 1
+        else:  # s[j] == s[i]
+            p[i] = j + 1
+            i += 1
+            j += 1
 
-    if len_pattern > len_search:
-        return occurrences
-
-    start = 0
-
-    while True:
-        pos = find(search, pattern, start)
-
-        if pos == -1:
-            break
-
-        occurrences.append(pos)
-        start = pos
-
-    return occurrences
-
-
-def find(search: list, pattern: list, start: int) -> int:
-    result = -1
-
-    if start >= len(search):
-        return result
-
-    if len(search) - start < len(pattern):
-        return result
-
-    for pos in range(start, len(search) - len(pattern) + 1):
-        shift = None
-        match = True
-
-        for offset in range(len(pattern)):
-            if shift is None:
-                shift = pattern[offset] - search[pos]
-
-            if search[pos + offset] + shift != pattern[offset]:
-                match = False
-                break
-
-        if match:
-            result = pos + 1
-            break
-
-    return result
+    return p
 
 
 if __name__ == '__main__':
